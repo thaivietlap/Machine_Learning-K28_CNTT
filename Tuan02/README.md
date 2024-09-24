bae8d09164078f12137019f9b54bf20db814f5b7
thaivietlap <144910441+thaivietlap@users.noreply.github.com>
2024-09-24 13:41:51 +0700

    update

--- Tuan02/README.md
+++ Tuan02/README.md
@@ -1,1 +1,5 @@
-Ngôn ngữ sử dụng là Python và html
+# Đây là kết quả của bài 1
+![alt text](image-1.png)
+![alt text](image-2.png)
+# Đây là kết quả của bài 2
+![alt text](image-3.png)
--- Tuan02/index.py
+++ Tuan02/index.py
@@ -3,7 +3,6 @@ app= Flask(__name__)
 
 @app.route("/")
 def hello_word():
-    return "<h1>Thái Viết Lập</h1>"
-
+    return "<h1>Thái Viết Lập - 2274802010473</h1>"
 if __name__=="__main__":
     app.run(debug=True)
